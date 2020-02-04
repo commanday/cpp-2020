9c2bfc59326bacb7d3193386fd8c9a3cc2609b12
commanday <60590616+commanday@users.noreply.github.com>
2020-02-04 08:30:56 +0800

    Update bookID.cpp

--- bookID.cpp
+++ bookID.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 void printBook( struct Books book );
  
-// 声明一个结构体类型 Books 
 struct Books
 {
    char  title[50];
@@ -15,25 +14,21 @@ struct Books
  
 int main( )
 {
-   Books Book1;        // 定义结构体类型 Books 的变量 Book1
-   Books Book2;        // 定义结构体类型 Books 的变量 Book2
+   Books Book1;         
+   Books Book2;            
  
-    // Book1 详述
    strcpy( Book1.title, "C++ 教程");
    strcpy( Book1.author, "Runoob"); 
    strcpy( Book1.subject, "编程语言");
    Book1.book_id = 12345;
  
-   // Book2 详述
    strcpy( Book2.title, "CSS 教程");
    strcpy( Book2.author, "Runoob");
    strcpy( Book2.subject, "前端技术");
    Book2.book_id = 12346;
  
-   // 输出 Book1 信息
    printBook( Book1 );
  
-   // 输出 Book2 信息
    printBook( Book2 );
  
    return 0;
