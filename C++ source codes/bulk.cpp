40199c673b762a3546e5b43a7134994be38a8d00
commanday <60590616+commanday@users.noreply.github.com>
2020-02-04 18:03:24 +0800

    Create bulk.cpp

---
+++ C++ source codes/bulk.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+	float fWidth = 3;
+	float fHeight = 2;
+	double dArea = 1;
+ 
+	cin >> fWidth >> fHeight;
+	dArea = fWidth * fHeight;
+ 
+	cout << "End：" << dArea << endl;
+	return 0;
+}
