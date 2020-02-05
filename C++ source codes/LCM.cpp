e12b1d3ef48c692f3f8fc84a79acb204f0b3871b
commanday <60590616+commanday@users.noreply.github.com>
2020-02-05 19:04:23 +0800

    Create LCM.cpp

---
+++ C++ source codes/LCM.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n1, n2, max;
+	n1 = 6;
+	n2 = 5;
+ 
+    cin >> n1 >> n2;
+    
+  
+    max = (n1 > n2) ? n1 : n2;
+ 
+    do
+    {
+        if (max % n1 == 0 && max % n2 == 0)
+        {
+            cout << "LCM = " << max;
+            break;
+        }
+        else
+            ++max;
+    } while (true);
+    
+    return 0;
+}
