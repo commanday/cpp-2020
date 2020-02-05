a6b3eabe091e6dca212f2ce2e4b0607d6ee5aef5
commanday <60590616+commanday@users.noreply.github.com>
2020-02-05 19:39:41 +0800

    Create maxinum_common_factor.cpp

---
+++ C++ source codes/maxinum_common_factor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ int main()
+{
+    int n1, n2;
+    n1 = 2;
+    n2 = 10;
+    cin >> n1 >> n2;
+    while(n1 != n2)
+    {
+        if(n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+    cout << "HCF = " << n1;
+    return 0;
+}
