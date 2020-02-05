9a818665988519973ded1c14b5c14c53e64696cc
commanday <60590616+commanday@users.noreply.github.com>
2020-02-05 18:55:34 +0800

    Update time2.cpp

--- C++ source codes/time2.cpp
+++ C++ source codes/time2.cpp
@@ -11,12 +11,12 @@ int main ()
    struct tm *info;
  
    time(&rawtime);
-   /* 获取 GMT 时间 */
+ 
    info = gmtime(&rawtime );
    
-   printf("当前的世界时钟：\n");
-   printf("伦敦：%2d:%02d\n", (info->tm_hour+BST)%24, info->tm_min);
-   printf("中国：%2d:%02d\n", (info->tm_hour+CCT)%24, info->tm_min);
+   printf("Current world clock：\n");
+   printf("London：%2d:%02d\n", (info->tm_hour+BST)%24, info->tm_min);
+   printf("China：%2d:%02d\n", (info->tm_hour+CCT)%24, info->tm_min);
  
    return(0);
 }
