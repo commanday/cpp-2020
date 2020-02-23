383a11135ebb4b5debfb10ee056edded22314124
commanday <60590616+commanday@users.noreply.github.com>
2020-02-23 17:06:12 +0800

    Create !.cpp

---
+++ C++ source codes/!.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n, sum = 0;
+    n = 3;
+ 
+    cout << "输入一个正整数: ";
+    cin >> n;
+ 
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+ 
+    cout << "Sum = " << sum;
+    return 0;
+}
